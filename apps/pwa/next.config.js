5458d3c229797954f8b468ced42a7fbb9b176db5
Saurav Nepal <antoneyosaurav111@gmail.com>
2025-02-27 22:07:26 +0545

    fixed

--- apps/pwa/next.config.js
+++ apps/pwa/next.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const withPWA = require('next-pwa')({
-    disable: process.env['NODE_ENV'] === 'development',
+    disable: true,
     dest: 'public',
     mode: 'production',
     skipWaiting: false,
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const withPWA = require('next-pwa')({
-    disable: process.env['NODE_ENV'] === 'development',
+    // disable: process.env['NODE_ENV'] === 'development',
+    disable: true,
     dest: 'public',
     mode: 'production',
     skipWaiting: false,
