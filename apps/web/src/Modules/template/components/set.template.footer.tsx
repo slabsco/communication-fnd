bfff07c020c600984dd027950425750e17c966fa
Saurav Nepal <antoneyosaurav111@gmail.com>
2025-10-14 21:15:07 +0545

    fixed footer

--- apps/web/src/Modules/template/components/set.template.footer.tsx
+++ apps/web/src/Modules/template/components/set.template.footer.tsx
@@ -4,15 +4,24 @@ import { useTemplate } from '../template.context';
 
 const SetTemplateFooter = () => {
     const { state, dispatch } = useTemplate();
+
     const { text } =
         state.components?.find((_component) => _component?.type === 'FOOTER') ||
         {};
+
     return (
         <InputField
             placeholder={'Enter Text'}
             label='Footer'
             maxLength={60}
-            onChange={(v) => dispatch({ type: 'UPDATE_FOOTER', payload: v })}
+            onChange={(v) =>
+                dispatch({
+                    type: 'UPDATE_FOOTER',
+                    payload: {
+                        text: v,
+                    },
+                })
+            }
             value={text}
         />
     );
