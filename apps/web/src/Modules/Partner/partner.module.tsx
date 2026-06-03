0009892b153d5b45d41fc9c06adef2bdd32164f9
Saurav N <antoneyosaurav111@gmail.com>
2026-06-03 16:01:25 +0545

    fixed scroll

--- apps/web/src/Modules/Partner/partner.module.tsx
+++ apps/web/src/Modules/Partner/partner.module.tsx
@@ -2,7 +2,7 @@ import PartnerNavbar from './navbar/partner.navbar';
 
 const PartnerModuleWrapper = ({ children }) => {
     return (
-        <main className='overflow-hidden h-screen bg-white col-flex'>
+        <main className='h-screen bg-white col-flex'>
             <PartnerNavbar />
             <div className='flex-1 bg-base-200'>{children}</div>
         </main>
